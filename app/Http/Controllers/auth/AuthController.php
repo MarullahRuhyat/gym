95bb205af9a47c6d68be5e4cf596e6c13b3b2ff9
rizkykamil <rabbaniikamil@gmail.com>
2024-07-15 13:29:26 +0700

    Update loginAdminIndex to handle remember me functionality

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -2,22 +2,40 @@
 
 namespace App\Http\Controllers\auth;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class AuthController extends Controller
 {
-    public function loginAdminIndex()
+    public function loginAdminIndex(Request $request)
     {
-        return view('auth.login-admin');
+        $phone_number = $request->cookie('phone_number');
+        $password = $request->cookie('password');
+        $remember = $request->cookie('remember_me');
+        if($remember == '1'){
+            $remember = $request->has('remember_me');
+        }
+        return view('auth.login-admin', compact('phone_number', 'password', 'remember'));
     }
 
     function loginAdminProcess(Request $request)
     {
+        $remember = $request->has('remember_me') ? true : false;
         $credentials = $request->only('phone_number', 'password');
         if (Auth::attempt($credentials)) {
+            if ($remember == true) {
+                Cookie::queue('phone_number', $request->phone_number, 60 * 24 * 30);
+                Cookie::queue('password', $request->password, 60 * 24 * 30); 
+            }
+            elseif ($remember == false) {
+                Cookie::queue(Cookie::forget('phone_number'));
+                Cookie::queue(Cookie::forget('password'));
+            }
+
+
             if (Auth::user()->role == 'admin') {
                 return redirect()->route('admin_dashboard');
             } elseif (Auth::user()->role == 'personal trainer') {

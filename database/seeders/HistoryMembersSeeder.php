5ffbc9888bb37f5021bba1b8035042bea2b1af23
rizkykamil <rabbaniikamil@gmail.com>
2024-07-20 18:54:10 +0700

    chore: Refactor HistoryMembersSeeder to improve code readability

--- database/seeders/HistoryMembersSeeder.php
+++ database/seeders/HistoryMembersSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class HistoryMembersSeeder extends Seeder
 {

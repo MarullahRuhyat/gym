213a8cc86223bca5242eee440c69d2533d09ac4d
rizkykamil <rabbaniikamil@gmail.com>
2024-07-22 03:02:12 +0700

    chore: Refactor Payment model and update Midtrans configuration

--- resources/views/member/membership/subscribed-package.blade.php
+++ resources/views/member/membership/subscribed-package.blade.php
@@ -4,50 +4,52 @@ Subscribed Package
 @endsection
 @section('content')
 <div class="row">
-    <div class="col-md-4">
-        @if(count($membership_payments) == 0)
+    @if(count($membership_payments) == null)
+    <div class="col-md-12">
         <p>No Subscribed Package</p>
         <a href="{{ route('member.package') }}" class="btn btn-primary">Buy Membership</a>
-        @else
+    </div>
+    @else
         @foreach($membership_payments as $membership)
-        <div class="card rounded-4">
-            <div class="card-header">
-                <h5 style="margin-top:10px;" class="card-title mb-3">{{ ucwords($membership->name) }}</h5>
-            </div>
-            <div class="card-body">
-                <div class="row g-0">
-                    <div class="col-md-4 border-end">
-                        <div class="p-3">
-                            <img src="{{ URL::asset('build/images/02.png') }}" class="w-100 rounded h-100" alt="...">
+        <div class="col-md-4">
+            <div class="card rounded-4">
+                <div class="card-header">
+                    <h5 style="margin-top:10px;" class="card-title mb-3">{{ ucwords($membership->name) }}</h5>
+                </div>
+                <div class="card-body">
+                    <div class="row g-0">
+                        <div class="col-md-4 border-end">
+                            <div class="p-3">
+                                <img src="{{ URL::asset('build/images/02.png') }}" class="w-100 rounded h-100" alt="...">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <input type="hidden" name="package_id" id="package_id" value="">
-                            <p class="card-text">{{ $membership->description }}</p>
-                            <p class="card-text">Duration: {{ $membership->duration_in_days }} Days</p>
-                            <p class="card-text">Starting: {{ $membership->start_date }}</p>
-                            <p class="card-text">Ending: {{ $membership->end_date }}</p>
-                            <p class="card-text">Personal Trainer Qouta: {{ $membership->personal_trainer_quota }} / {{ $membership->personal_trainer_quota }}</p>
-                            <p class="card-text">Price: Rp.{{ $membership->price }}</p>
-                            @if($membership->is_active)
-                                <button class="btn btn-primary" disabled>Active</button>
-                            @else
-                                <button class="btn btn-secondary" disabled>Inactive</button>
-                            @endif
-                            @if($membership->status == 'pending')
-                                <button class="btn btn-warning" style="color:white">Check Payment</button>
-                            @endif
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <input type="hidden" name="package_id" id="package_id" value="">
+                                <p class="card-text">{{ $membership->description }}</p>
+                                <p class="card-text">Duration: {{ $membership->duration_in_days }} Days</p>
+                                <p class="card-text">Starting: {{ $membership->start_date }}</p>
+                                <p class="card-text">Ending: {{ $membership->end_date }}</p>
+                                <p class="card-text">Personal Trainer Quota: {{ $membership->personal_trainer_quota }} / {{ $membership->personal_trainer_quota }}</p>
+                                <p class="card-text">Price: Rp.{{ $membership->price }}</p>
+                                @if($membership->is_active)
+                                    <button class="btn btn-primary" disabled>Active</button>
+                                @else
+                                    <button class="btn btn-secondary" disabled>Inactive</button>
+                                @endif
+                                @if($membership->status == 'pending')
+                                    <button class="btn btn-warning" style="color:white">Check Payment</button>
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
         @endforeach
-        @endif
-    </div>
+    @endif
 </div>
+
 @endsection
 @push('script')
 <!--plugins-->

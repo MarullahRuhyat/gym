620510e4bc5348b7e6d63efec7ee6cf13ccec0f6
rizkykamil <rabbaniikamil@gmail.com>
2024-08-20 22:16:53 +0700

    update salary pt default 0

--- database/migrations/2024_07_18_222141_add_salary_pt_to_users_table.php
+++ database/migrations/2024_07_18_222141_add_salary_pt_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('salary_pt')->nullable();
+            $table->unsignedBigInteger('salary_pt')->default(0);
         });
     }
 

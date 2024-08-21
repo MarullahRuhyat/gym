5d9f9b0fa1eb523bb599d6e38f95f82b282d680e
rizkykamil <rabbaniikamil@gmail.com>
2024-08-21 19:44:56 +0700

    fix last day

--- app/Http/Controllers/Member/AuthController.php
+++ app/Http/Controllers/Member/AuthController.php
@@ -40,8 +40,7 @@ class AuthController extends Controller
                 'otp' => $otp,
                 'otp_expired_at' => $expired_at,
             ]);
-            $send = new sendNotif();
-            $curl = $send->sendWA($phone_number,'Flozors Gym: Gunakan kode OTP ' . $otp . ', UNTUK LOGIN KE AKUN ANDA. Berlaku selama 5 menit. JANGAN pernah membagikan kode ini kepada orang lain, termasuk staf Flozors Gym.');
+            $curl = $this->sendWA($phone_number,'Flozors Gym: Gunakan kode OTP ' . $otp . ', UNTUK LOGIN KE AKUN ANDA. Berlaku selama 5 menit. JANGAN pernah membagikan kode ini kepada orang lain, termasuk staf Flozors Gym.');
         }
 
         $data = [
@@ -54,6 +53,40 @@ class AuthController extends Controller
         ];
 
         return response()->json($data, 200);
+    }
+
+    private function sendWA($mobileNumber, $message)
+    {
+
+        $url = "https://jogja.wablas.com";
+        $token = "SujEXjKi5MEvVWebuRK17sG4H69mKzZwFD4Uca7HPrPwNiQawGLJ4ShA5uCCaUtv";
+
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $url,
+        ]);
+        $response = $client->post('/api/send-message', [
+            'form_params' => [
+                'phone' => $mobileNumber,
+                'message' => $message
+            ],
+            'headers' => [
+                'Accept'     => 'application/json',
+                'Content-Type' => 'application/x-www-form-urlencoded',
+                'Authorization' => $token
+            ]
+        ]);
+
+        $body = json_decode($response->getBody(), true);
+        if($body['status'] == true) {
+            // return true;
+            if($response->getStatusCode() != 200) {
+                return false;
+            }else{
+                return true;
+            }
+        }
+        return false;
+
     }
 
     public function verify_otp($phone_number)

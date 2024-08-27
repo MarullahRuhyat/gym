c15f2a346573fdfb40f026e52f0d26326ba354fb
hhaihen <hnytyas72@student.uns.ac.id>
2024-08-27 23:37:47 +0700

    merge: fix conflict web.php

--- resources/views/admin/payment_cash.blade.php
+++ resources/views/admin/payment_cash.blade.php
@@ -28,6 +28,20 @@
             
         </div>
     </div>
+<<<<<<< HEAD
+=======
+    <div class="col-md-6">
+        <div class="card rounded-4">
+            <div class="card-body">
+                <p class="mb-4">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
+                    classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor
+                    at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
+                    Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the
+                    undoubtable source.</p>
+            </div>
+        </div>
+    </div>
+>>>>>>> 92d1453 (cash payment)
 </div>
     
     

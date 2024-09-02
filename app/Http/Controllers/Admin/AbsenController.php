a65207384e57eff5620e645648aa659fe6b9ee50
rizkykamil <rabbaniikamil@gmail.com>
2024-09-02 22:41:52 +0700

    update absent fix search

--- app/Http/Controllers/Admin/AbsenController.php
+++ app/Http/Controllers/Admin/AbsenController.php
@@ -23,10 +23,12 @@ class AbsenController extends Controller
         $data_member = AbsentMember::join('users as member', 'absent_members.member_id', '=', 'member.id')
             ->leftJoin('users as trainer', 'absent_members.personal_trainer_id', '=', 'trainer.id')
             ->whereDate('absent_members.date', $today)
-            ->select('member.name as member_name', 
-                        'member.phone_number', 
-                        'absent_members.*', 
-                        'trainer.name as trainer_name')
+            ->select(
+                'member.name as member_name',
+                'member.phone_number',
+                'absent_members.*',
+                'trainer.name as trainer_name'
+            )
             ->get();
 
         return view('admin.absen', compact('data_member', 'dataLatihan'));
@@ -35,32 +37,35 @@ class AbsenController extends Controller
     {
         $searchName = $request->input('searchName');
         $searchDate = $request->input('searchDate');
+
         $query = AbsentMember::join('users as member', 'absent_members.member_id', '=', 'member.id')
-        ->leftJoin('users as trainer', 'absent_members.personal_trainer_id', '=', 'trainer.id')
-        ->select('member.name as member_name', 
-                    'member.phone_number', 
-                    'absent_members.*', 
-                    'trainer.name as trainer_name');
+            ->leftJoin('users as trainer', 'absent_members.personal_trainer_id', '=', 'trainer.id');
 
         if ($searchName) {
-            $query->where('users.name', 'like', '%' . $searchName . '%');
+            $query->where('member.name', 'like', '%' . $searchName . '%');
         }
 
         if ($searchDate) {
-            $query->whereDate('date', $searchDate);
+            $query->whereDate('absent_members.date', $searchDate);
         } else {
             $today = Carbon::today()->toDateString();
-            $query->whereDate('date', $today);
+            $query->whereDate('absent_members.date', $today);
         }
 
-        $data_member = $query->select('users.name', 'users.phone_number', 'absent_members.*')->get();
+        $data_member = $query->select(
+            'member.name as member_name',
+            'member.phone_number',
+            'absent_members.*',
+            'trainer.name as trainer_name'
+        )
+            ->get();
 
         if ($request->ajax()) {
             return response()->json($data_member);
         }
 
-
         $dataLatihan = JenisLatihan::all();
         return view('admin.absen', compact('data_member', 'dataLatihan'));
     }
+
 }
